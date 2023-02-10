54a270b67479da30c492400d0988a4198a1f26c7
Stan Lo <stan001212@gmail.com>
2023-02-10 22:02:23 +0000

    Improve tracing commands' regsitration

--- lib/ruby_tracer/irb.rb
+++ lib/ruby_tracer/irb.rb
@@ -1,12 +1,34 @@
 require "irb/cmd/nop"
 require "irb"
 
+module Tracer
+  def self.register_irb_commands
+    ec = IRB::ExtendCommandBundle.instance_variable_get(:@EXTEND_COMMANDS)
+
+    [
+      [:trace, :Trace, nil, [:trace, IRB::ExtendCommandBundle::OVERRIDE_ALL]],
+      [
+        :trace_call,
+        :TraceCall,
+        nil,
+        [:trace_call, IRB::ExtendCommandBundle::OVERRIDE_ALL]
+      ],
+      [
+        :trace_exception,
+        :TraceException,
+        nil,
+        [:trace_exception, IRB::ExtendCommandBundle::OVERRIDE_ALL]
+      ]
+    ].each do |ecconfig|
+      ec.push(ecconfig)
+      IRB::ExtendCommandBundle.def_extend_command(*ecconfig)
+    end
+  end
+end
+
 module IRB
   module ExtendCommand
-    class Trace < Nop
-      category "Tracing"
-      description "Trace the target object in the given expression. `trace [target,] expression`"
-
+    class TraceCommand < Nop
       class << self
         def transform_args(args)
           # Return a string literal as is for backward compatibility
@@ -17,6 +39,11 @@ module IRB
           end
         end
       end
+    end
+
+    class Trace < TraceCommand
+      category "Tracing"
+      description "Trace the target object (or self) in the given expression. Usage: `trace [target,] <expression>`"
 
       def execute(*args)
         args = args.first.split(/,/, 2)
@@ -33,17 +60,31 @@ module IRB
                 "wrong number of arguments (given #{args.size}, expected 1..2)"
         end
 
-        Tracer.trace(target) { eval(expression, irb_context.workspace.binding) }
+        b = irb_context.workspace.binding
+        Tracer.trace(target) { eval(expression, b) }
+      end
+    end
+
+    class TraceCall < TraceCommand
+      category "Tracing"
+      description "Trace method calls in the given expression. Usage: `trace_call <expression>`"
+
+      def execute(expression)
+        b = irb_context.workspace.binding
+        Tracer.trace_call { eval(expression, b) }
+      end
+    end
+
+    class TraceException < TraceCommand
+      category "Tracing"
+      description "Trace exceptions in the given expression. Usage: `trace_exception <expression>`"
+
+      def execute(expression)
+        b = irb_context.workspace.binding
+        Tracer.trace_exception { eval(expression, b) }
       end
     end
   end
 end
 
-ec = IRB::ExtendCommandBundle.instance_variable_get(:@EXTEND_COMMANDS)
-
-[
-  [:trace, :Trace, nil, [:trace, IRB::ExtendCommandBundle::OVERRIDE_ALL]]
-].each do |ecconfig|
-  ec.push(ecconfig)
-  IRB::ExtendCommandBundle.def_extend_command(*ecconfig)
-end
+Tracer.register_irb_commands
